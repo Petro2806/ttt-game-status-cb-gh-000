238cd9a897b678a51ed270c29a3ca7b6bc46abf6
Petro Tarnavskyy <petro.tarnavskyy@lnu.edu.ua>
2018-05-21 17:32:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,8 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=[[0,1,2],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
 def won?(board)
-  WIN_COMBINATIONS=[[0,1,2],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
   WIN_COMBINATIONS.each do |cur|
     if(board[cur[0]]=="X" && board[cur[1]]=="X" && board[cur[2]]=="X")
       return cur
